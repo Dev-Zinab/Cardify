5c408f5f3d2ff03b20792f0c4321770ccf06fb35
alhanoufalhamied <129813000+alhanoufalhamied@users.noreply.github.com>
2024-01-28 15:36:57 +0300

    Initial Commit

---
+++ Cardify/CardifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardifyApp.swift
+//  Cardify
+//
+//  Created by alhanouf alhamied on 16/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct CardifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cardify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cardify
+//
+//  Created by alhanouf alhamied on 16/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
